242e300a6eef9de613418b39b9c935f13f2e1841
Kevin Winter <KevinW@ppedv.de>
2020-02-24 18:17:56 +0100

    Tag1 (Bis Modul04)

---
+++ AspNetCoreKurs_Berlin/Pages/Modul03/IndexA.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCoreKurs_Berlin.Modul03
+{
+    public class IndexAModel : PageModel
+    {
+        public int MyProperty { get; set; }
+        
+        public void OnGet()
+        {
+            MyProperty = 7;
+        }
+    }
+}
---
+++ DepencyInjectionSample/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DepencyInjectionSample
+{
+    public interface ICar
+    {
+        string Marke { get; set; }
+        string Model { get; set; }
+        int Baujahr { get; set; }
+    }
+    public class Car : ICar
+    {
+        public string Marke 
+        { 
+            get; set; 
+        }
+
+        public string Model 
+        { 
+            get; set; 
+        }
+        public int Baujahr 
+        { 
+            get; set ; 
+        }
+    }
+
+    public interface ICarService
+    {
+        IList<ICar> Cars { get; set; }
+
+        ICar GetCarById(int Id);
+    }
+
+    public class CarService : ICarService
+    {
+        public IList<ICar> Cars { get; set; }
+
+        public ICar GetCarById(int Id)
+        {
+            ICar currentCar = Cars.Single(n => n.Baujahr > 2000);
+
+            //return currentCar; 
+            return new Car { Marke = "VW", Model = "Passat", Baujahr = 2001 };
+        }
+    }
+}
